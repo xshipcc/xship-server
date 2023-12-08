39d8e450b1194c45f443525425a60aec85bcff74
magix <minyuanyang@qq.com>
2023-12-08 13:46:39 +0800

    更新

--- rpc/doc/sql/uav_plan.sql
+++ rpc/doc/sql/uav_plan.sql
@@ -3,7 +3,7 @@ create table uav_plan
     id                bigint auto_increment comment '编号'
         primary key,
     uav_id         bigint                              not null comment '无人机id',
-    plan       varchar(32)                        not null comment '定时字符串',
+    plan       varchar(256)                        not null comment '定时字符串',
     fly_id        bigint                              not null comment '巡检路线id',
     create_time       timestamp default CURRENT_TIMESTAMP null comment '飞行开始时间'
 )
