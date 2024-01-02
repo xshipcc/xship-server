d8fa799f52b22550facae3e8360769c8c5236cef
magix <minyuanyang@qq.com>
2024-01-02 15:57:23 +0800

    更新

--- rpc/doc/sql/uav_fly_history.sql
+++ rpc/doc/sql/uav_fly_history.sql
@@ -5,6 +5,8 @@ create table uav_fly_history
     uav_id         bigint                              not null comment '无人机id',
     fly_id        bigint                              not null comment '巡检路线id',
     operator       varchar(16)                        not null comment '操作者',
+    lat    double                                 not null comment '纬度',
+    lon    double                                 not null comment '经度',
     create_time    timestamp default CURRENT_TIMESTAMP null comment '飞行开始时间',
     end_time       timestamp default CURRENT_TIMESTAMP null comment '飞行结束时间'
 )
