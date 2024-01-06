170a848528998f00e23ef6374b1d3b366af3e965
magix <minyuanyang@qq.com>
2024-01-06 12:18:25 +0800

    222

--- rpc/uav/uav.go
+++ rpc/uav/uav.go
@@ -31,7 +31,7 @@ var configFile = flag.String("f", "rpc/uav/etc/uav.yaml", "the config file")
 func runUavFlight(ip string, port int, rport int, Hangar_ip string, Hangar_port int, Hangar_rport int, cameraip string, cameraport int, url string) {
 	//execcmd := fmt.Sprintf("python3  drone_projects/client.py %s %d %d  %s %d %s %d %d ", ip, port, rport, cameraip, cameraport, Hangar_ip, Hangar_port, Hangar_rport)
 
-	cmd := exec.Command("python3", "drone_projects/client.py", ip, strconv.Itoa(port), strconv.Itoa(rport), cameraip, strconv.Itoa(cameraport), Hangar_ip, strconv.Itoa(Hangar_port), strconv.Itoa(Hangar_rport), url)
+	cmd := exec.Command("python3", "/javodata/drone_projects/client.py", ip, strconv.Itoa(port), strconv.Itoa(rport), cameraip, strconv.Itoa(cameraport), Hangar_ip, strconv.Itoa(Hangar_port), strconv.Itoa(Hangar_rport), url)
 
 	fmt.Println("cmd -> ", cmd)
 	// cmd := exec.Command(execcmd)
@@ -125,53 +125,65 @@ func main() {
 		cmp := strings.Compare(ctlitem.Cmd, "fly")
 		if cmp == 0 {
 
-			res, err := ctx.UavFlyHistoryModel.Insert(sctx, &uavmodel.UavFlyHistory{
-				UavId:      ctlitem.UavId,
-				FlyId:      ctlitem.FlyId,
-				Operator:   ctlitem.FlyOp,
-				CreateTime: time.Now(),
-				EndTime:    time.Now(),
-				Lat:        ctlitem.Lat,
-				Lon:        ctlitem.Lon,
-				Alt:        ctlitem.Alt,
-			})
-			if err != nil {
-				fmt.Printf("添加历史  err:%s\n", err)
-			}
-			fly, err := ctx.UavFlyModel.FindOne(sctx, ctlitem.FlyId)
-			if err != nil {
-				fmt.Printf("航线  err:%s\n", err)
-			}
-			lastid, _ := res.LastInsertId()
-			var flydata uavlient.UavFlyData
-			flydata.Cmd = "dofly"
-			flydata.Data = fly.Data
-			flydata.Historyid = lastid
+			try_catch.Try(func() {
+				res, err := ctx.UavFlyHistoryModel.Insert(sctx, &uavmodel.UavFlyHistory{
+					UavId:      ctlitem.UavId,
+					FlyId:      ctlitem.FlyId,
+					Operator:   ctlitem.FlyOp,
+					CreateTime: time.Now(),
+					EndTime:    time.Now(),
+					Lat:        ctlitem.Lat,
+					Lon:        ctlitem.Lon,
+					Alt:        ctlitem.Alt,
+				})
+				if err != nil {
+					fmt.Printf("添加历史  err:%s\n", err)
+				}
+				fly, err := ctx.UavFlyModel.FindOne(sctx, ctlitem.FlyId)
+				if err != nil {
+					fmt.Printf("航线  err:%s\n", err)
+				}
+				lastid, _ := res.LastInsertId()
+				var flydata uavlient.UavFlyData
+				flydata.Cmd = "dofly"
+				flydata.Data = fly.Data
+				flydata.Historyid = lastid
 
-			flysend, err := json.Marshal(flydata)
-			// text := fmt.Sprintf('{"cmd":"dofly","data":%s ,"historyid": %d}', flydata, lastid)
+				flysend, err := json.Marshal(flydata)
+				// text := fmt.Sprintf('{"cmd":"dofly","data":%s ,"historyid": %d}', flydata, lastid)
 
-			ctx.MMQServer.Publish("control", flysend)
-
+				ctx.MMQServer.Publish("control", flysend)
+			}).DefaultCatch(func(err error) {
+				fmt.Println("---->catch", err)
+			}).Finally(func() {
+				fmt.Println("---->finally")
+			}).Do()
 			//start ai process;
 		}
 		cmp = strings.Compare(ctlitem.Cmd, "fly_over")
 		if cmp == 0 {
 			sctx := context.Background()
-			item, err := ctx.UavFlyHistoryModel.FindOne(sctx, ctlitem.HistoryId)
-			if err != nil {
-				fmt.Printf("parse  err:%s\n", err)
-			}
+			try_catch.Try(func() {
 
-			item.EndTime = time.Now()
+				item, err := ctx.UavFlyHistoryModel.FindOne(sctx, ctlitem.HistoryId)
+				if err != nil {
+					fmt.Printf("parse  err:%s\n", err)
+				}
 
-			err = ctx.UavFlyHistoryModel.Update(sctx, item)
-			if err != nil {
-				fmt.Printf("parse  err:%s\n", err)
-			}
-			if err != nil {
-				fmt.Printf("parse  err:%s\n", err)
-			}
+				item.EndTime = time.Now()
+
+				err = ctx.UavFlyHistoryModel.Update(sctx, item)
+				if err != nil {
+					fmt.Printf("parse  err:%s\n", err)
+				}
+				if err != nil {
+					fmt.Printf("parse  err:%s\n", err)
+				}
+			}).DefaultCatch(func(err error) {
+				fmt.Println("---->catch", err)
+			}).Finally(func() {
+				fmt.Println("---->finally")
+			}).Do()
 		}
 
 		// cmp = strings.Compare(ctlitem.Cmd, "autofly")
@@ -231,73 +243,33 @@ func main() {
 		cmp = strings.Compare(ctlitem.Cmd, "corn")
 		if cmp == 0 {
 			fmt.Printf("load paln corn  .................")
+			try_catch.Try(func() {
 
-			ctx.CornServer.Stop()
-			// count, _ := ctx.UavPlanModel.Count(ctx)
-			all, err := ctx.UavPlanModel.FindAll(ctx, 1, 1)
-			if err != nil {
-				fmt.Printf("load paln error  err:%s\n", err)
-			}
-			for _, dict := range *all {
+				ctx.CornServer.Stop()
+				// count, _ := ctx.UavPlanModel.Count(ctx)
+				all, err := ctx.UavPlanModel.FindAll(ctx, 1, 1)
+				if err != nil {
+					fmt.Printf("load paln error  err:%s\n", err)
+				}
+				for _, dict := range *all {
 
-				ctx.CornServer.AddFunc(dict.Plan, func() {
-					fmt.Println("fly fly.  go go go !")
-					text := fmt.Sprintf("{'cmd':'fly','uav_id': %d,'fly_id': %d}", dict.UavId, dict.FlyId)
-					ctx.MMQServer.Publish("control", text)
+					ctx.CornServer.AddFunc(dict.Plan, func() {
+						fmt.Println("fly fly.  go go go !")
+						text := fmt.Sprintf("{'cmd':'fly','uav_id': %d,'fly_id': %d}", dict.UavId, dict.FlyId)
+						ctx.MMQServer.Publish("control", text)
 
-				})
-				fmt.Printf("load paln :%s\n", dict.Plan)
-			}
+					})
+					fmt.Printf("load paln :%s\n", dict.Plan)
+				}
 
-			ctx.CornServer.Start()
+				ctx.CornServer.Start()
+			}).DefaultCatch(func(err error) {
+				fmt.Println("---->catch", err)
+			}).Finally(func() {
+				fmt.Println("---->finally")
+			}).Do()
 		}
 	}
-
-	// count, _ := ctx.UavDeviceModel.Count(ctx)
-	// fmt.Printf("uav device is err:%d\n", count)
-	// sctx := context.Background()
-	// all, err := ctx.UavDeviceModel.FindAll(sctx, 0, 10)
-	// fmt.Printf("is err:%s\n", err)
-	// itemList := *all
-	// if len(itemList) > 0 {
-	// 	fmt.Printf("is ssss:%s\n", itemList[0].Ip)
-	// 	runUavFlight(itemList[0].Ip, int(itemList[0].Port), int(itemList[0].RPort), itemList[0].HangarIp, int(itemList[0].HangarPort),
-	// 		int(itemList[0].HangarRport), itemList[0].CamIp, int(itemList[0].CamPort))
-	// }
-
-	// var list []*types.ListUavDeviceData
-
-	// for _, dict := range *all {
-	// 	list = append(list, &types.ListUavDeviceData{
-	// 		Id:           dict.Id,
-	// 		Name:         dict.Name,
-	// 		Ip:           dict.Ip,
-	// 		Port:         dict.Port,
-	// 		R_Port:       dict.RPort,
-	// 		Hangar_ip:    dict.HangarIp,
-	// 		Hangar_port:  dict.HangarPort,
-	// 		Hangar_rport: dict.HangarRport,
-	// 		Cam_ip:       dict.CamIp,
-	// 		Cam_port:     dict.CamPort,
-	// 		Cam_url:      dict.CamUrl,
-	// 	})
-	// }
-
-	// runUavFlight(c.AIRPORT.IP, c.AIRPORT.PORT, c.AIRPORT.RPORT)
-
-	// handleUavFunc := func(source []byte) {
-	// 	logx.Errorf("Uav参数: %s", string(source))
-	// 	var alertitem uavmodel.UavFlyHistoryDetail
-
-	// 	err := json.Unmarshal(source, &alertitem)
-	// 	if err != nil {
-	// 		fmt.Printf("parse  err:%s\n", err)
-	// 	}
-	// 	_, err = ctx.UavDeviceModel.Insert(context.Background(), &alertitem)
-	// 	if err != nil {
-	// 		fmt.Printf("parse  err:%s\n", err)
-	// 	}
-	// }
 
 	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
 		uavlient.RegisterUavDeviceServiceServer(grpcServer, uavdeviceserviceServer.NewUavDeviceServiceServer(ctx))
@@ -313,8 +285,8 @@ func main() {
 	// ctx.MMQServer.Subscription("alert/#", handleAlertFunc)
 	ctx.MMQServer.Subscription("fly_control/#", handleCtlFunc)
 	// text := "{'cmd':'corn'}"
-	ctx.MMQServer.Publish("fly_control", "start_uav")
+	// ctx.MMQServer.Publish("fly_control", "start_uav")
 
-	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	fmt.Printf("Starting uav rpc server at %s...\n", c.ListenOn)
 	s.Start()
 }
