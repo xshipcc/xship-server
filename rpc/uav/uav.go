ba837eef52f2f57e63bbef2baae97a080359e267
magix <minyuanyang@qq.com>
2024-01-10 22:52:28 +0800

    更新

--- rpc/uav/uav.go
+++ rpc/uav/uav.go
@@ -29,9 +29,9 @@ import (
 
 var configFile = flag.String("f", "rpc/uav/etc/uav.yaml", "the config file")
 
-func runUavFlight(ip string, port int, rport int, Hangar_ip string, Hangar_port int, Hangar_rport int, cameraip string, cameraport int, zubo int) *exec.Cmd {
+func runUavFlight(ip string, port int, rport int, Hangar_ip string, Hangar_port int, Hangar_rport int, cameraip string, cameraport int, zubo int, network string, joystick string) *exec.Cmd {
 
-	cmd := exec.Command("python3", "/javodata/drone_projects/client.py", ip, strconv.Itoa(port), strconv.Itoa(rport), cameraip, strconv.Itoa(cameraport), Hangar_ip, strconv.Itoa(Hangar_port), strconv.Itoa(Hangar_rport), strconv.Itoa(zubo))
+	cmd := exec.Command("python3", "/javodata/drone_projects/client.py", ip, strconv.Itoa(port), strconv.Itoa(rport), cameraip, strconv.Itoa(cameraport), Hangar_ip, strconv.Itoa(Hangar_port), strconv.Itoa(Hangar_rport), strconv.Itoa(zubo), network, joystick)
 
 	fmt.Println("cmd -> ", cmd)
 	// cmd := exec.Command(execcmd)
@@ -262,7 +262,7 @@ func main() {
 						ctx.Cmd.Process.Kill()
 					}
 					ctx.Cmd = runUavFlight(oneuav.Ip, int(oneuav.Port), int(oneuav.RPort), oneuav.HangarIp, int(oneuav.HangarPort),
-						int(oneuav.HangarRport), oneuav.CamIp, int(oneuav.CamPort), int(oneuav.UavZubo))
+						int(oneuav.HangarRport), oneuav.CamIp, int(oneuav.CamPort), int(oneuav.UavZubo), oneuav.Network, oneuav.Joystick)
 				}
 			}).DefaultCatch(func(err error) {
 				fmt.Println("---->catch", err)
