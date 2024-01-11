2bb5d9fe65045c8e0db35ed7374a0d2fc4065151
magix <minyuanyang@qq.com>
2024-01-11 16:29:21 +0800

    更新

--- restart.sh
+++ restart.sh
@@ -1,26 +1,13 @@
-# !/bin/bash
+!/bin/bash
+#回到代码仓库
+cd ~/Server/
 
-#停止服务
-# pkill uav_oms
-# pkill uav_pms
-# pkill uav_sms
-# pkill uav_cms
 pkill uav_sys
 pkill uav_uav
-# pkill uav_ums
 pkill uav_api
 pkill MediaServer
 pkill nanomq 
 
-#删除容器
-
-# rm uav_oms
-# rm uav_pms
-# rm uav_sms
-# rm uav_cms
-rm uav_sys
-rm uav_uav
-rm uav_api
 
 
 #构建服务
@@ -41,45 +28,14 @@ go build
 mv uav ../../uav_uav
 cd ../../
 
-
-# #用户系统
-# cd rpc/ums
-# go build
-# mv ums ../../uav_ums
-# cd ../../
-
-
-
-# #用户系统
-# cd rpc/cms
-# go build
-# mv cms ../../uav_cms
-# cd ../../
-
-
-# #sms
-# cd rpc/sms
-# go build
-# mv sms ../../uav_sms
-# cd ../../
-
-# #pms
-# cd rpc/pms
-# go build
-# mv pms ../../uav_pms
-# cd ../../
-
-# #oms
-# cd rpc/oms
-# go build
-# mv oms ../../uav_oms
-# cd ../../
-
-
-
+#复制程序
+cp uav_* /javodata/ -rf
+cp drone_projects /javodata/ -rf
 #启动服务
+
+cd /javodata/
 ./MediaKit/MediaServer -d &
 nanomq start &
-./uav_uav &
-./uav_sys &
-./uav_api &
+nohup ./uav_sys &
+nohup ./uav_api &
+./uav_uav
