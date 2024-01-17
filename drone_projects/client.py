6da25830c4c9b203e381564ca7d4f476aaa78a05
magix <minyuanyang@qq.com>
2024-01-17 14:15:59 +0800

    geegnxin

--- drone_projects/client.py
+++ drone_projects/client.py
@@ -1414,7 +1414,7 @@ class UavThread(threading.Thread):
                 data, _ = self.sock.recvfrom(32)      # buffer size is 4096 bytes
                 databuffer+=data
           
-                
+            print("to offset"+offset)
             todata=bytes(bytearray(databuffer))
             print("to package : {}".format( todata))
             ctypes.memmove(ctypes.addressof(heartbeat), todata, ctypes.sizeof(heartbeat))
