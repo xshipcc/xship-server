8e3eb21e22be9407b668597cde7ebe0ecd06785d
magix <22333678@qq.com>
2024-01-25 16:23:46 +0800

    wanzhzeng

--- drone_projects/client.py
+++ drone_projects/client.py
@@ -169,28 +169,28 @@ class AutoThread(threading.Thread):
     
         time.sleep(5)
         # quit_time =0
-        # while(airport.airportdata.warehouse_status !=2):
-        #     if airport.airportdata.warehouse_status == 2: 
-        #         consolelog("舱盖已经打开")
-        #         break
-        #     quit_time +=1
-        #     if quit_time > 10:
-        #         consolelog("舱盖无法打开")
-        #         SendFlyOver(3,"舱盖无法打开")
-        #         return
-        #     time.sleep(2)
+        while(airport.airportdata.warehouse_status !=2):
+            if airport.airportdata.warehouse_status == 2: 
+                consolelog("舱盖已经打开")
+                break
+            quit_time +=1
+            if quit_time > 10:
+                consolelog("舱盖无法打开")
+                SendFlyOver(3,"舱盖无法打开")
+                return
+            time.sleep(2)
           
-        # #发送航线数据
-        # # print('无人机定位数据' + str(uav.uavdata.lon) + "  "+str(uav.uavdata.lat) )
-        # consolelog('无人机定位数据 : ' + str(uav.uavdata.lon) + "  "+str(uav.uavdata.lat) )
-        # quit_time =0
-        # while(uav.uavdata.lon != 0 and uav.uavdata.lat != 0 ):
-        #     quit_time +=1
-        #     if quit_time > 10:
-        #         consolelog("定位失败,无法起飞")
-        #         SendFlyOver(3,"定位失败,无法起飞")
-        #         return
-        #     time.sleep(1)
+        #发送航线数据
+        # print('无人机定位数据' + str(uav.uavdata.lon) + "  "+str(uav.uavdata.lat) )
+        consolelog('无人机定位数据 : ' + str(uav.uavdata.lon) + "  "+str(uav.uavdata.lat) )
+        quit_time =0
+        while(uav.uavdata.lon == 0 and uav.uavdata.lat == 0 ):
+            quit_time +=1
+            if quit_time > 10:
+                consolelog("定位失败,无法起飞")
+                SendFlyOver(3,"定位失败,无法起飞")
+                return
+            time.sleep(1)
         RunSelfCheck()
         consolelog("装订航线")
         # a =[path]
@@ -410,97 +410,97 @@ async def RunSelfCheck():
     global SelfCheck
     SelfCheck = 1
     #电池电压  (配置文件的电池电压参数)
-    # if SelfCheck == 1 :
-    #     if uav.uavdata.v < 44:
-    #         SelfCheck =0 
-    #         consolelog("电压自检失败")
-    #     else:
-    #         SelfCheck =1
-    #         consolelog("电压自检successfull")
+    if SelfCheck == 1 :
+        if uav.uavdata.v < 44:
+            SelfCheck =0 
+            consolelog("电压自检失败")
+        else:
+            SelfCheck =1
+            consolelog("电压自检successfull")
 
     
-    # # 定位状态
-    # if SelfCheck == 1 :
-    #     if uav.uavdata.offset_staus == 0:
-    #         SelfCheck =0 
-    #         consolelog("定位自检失败")
-    #     else:
-    #         SelfCheck = 1
-    #         consolelog("定位自检successfull")
+    # 定位状态
+    if SelfCheck == 1 :
+        if uav.uavdata.offset_staus == 0:
+            SelfCheck =0 
+            consolelog("定位自检失败")
+        else:
+            SelfCheck = 1
+            consolelog("定位自检successfull")
 
-    # # 丢星时间
-    # if SelfCheck == 1 :
-    #     if uav.uavdata.gps_lost > 0:
-    #         SelfCheck =0 
-    #         consolelog("丢星时间自检失败")
-    #     else:
-    #         SelfCheck = 1
-    #         consolelog("丢星successfull")
+    # 丢星时间
+    if SelfCheck == 1 :
+        if uav.uavdata.gps_lost > 0:
+            SelfCheck =0 
+            consolelog("丢星时间自检失败")
+        else:
+            SelfCheck = 1
+            consolelog("丢星successfull")
 
-    # # 俯仰角
-    # if SelfCheck == 1 :
-    #     if uav.uavdata.pitch/100 > 3:
-    #         SelfCheck =0 
-    #         consolelog("俯仰角自检失败")
-    #     else:
-    #         SelfCheck = 1
-    #         consolelog("俯仰角自检successfull")
+    # 俯仰角
+    if SelfCheck == 1 :
+        if uav.uavdata.pitch/100 > 3:
+            SelfCheck =0 
+            consolelog("俯仰角自检失败")
+        else:
+            SelfCheck = 1
+            consolelog("俯仰角自检successfull")
 
-    # # 横滚角
-    # if SelfCheck == 1 :
-    #     # consolelog(uav.uavdata.roll_angle)
-    #     if uav.uavdata.roll_angle/100 > 3:
-    #         SelfCheck =0 
-    #         consolelog("横滚角自检失败")
-    #     else:
-    #         SelfCheck = 1
-    #         consolelog("横滚角自检successfull")
+    # 横滚角
+    if SelfCheck == 1 :
+        # consolelog(uav.uavdata.roll_angle)
+        if uav.uavdata.roll_angle/100 > 3:
+            SelfCheck =0 
+            consolelog("横滚角自检失败")
+        else:
+            SelfCheck = 1
+            consolelog("横滚角自检successfull")
 
-    # # 磁罗盘连接 无人机遥测信息第71-72字节第二位是不是1 不是1则异常
-    # if SelfCheck == 1 :
-    #     if uav.mc != 1:
-    #         SelfCheck =0 
-    #         consolelog("磁罗盘连接自检失败")
-    #     else:
-    #         SelfCheck = 1
-    #         consolelog("磁罗连接successfull")
+    # 磁罗盘连接 无人机遥测信息第71-72字节第二位是不是1 不是1则异常
+    if SelfCheck == 1 :
+        if uav.mc != 1:
+            SelfCheck =0 
+            consolelog("磁罗盘连接自检失败")
+        else:
+            SelfCheck = 1
+            consolelog("磁罗连接successfull")
 
-    # # 磁罗盘测向和卫星测向偏差
-    # if SelfCheck == 1:
-    #     pod = Fight.Flight_Action()
-    #     data =pod.MagneticDeclination()
-    #     uav.Send(data) 
-    #     data =pod.Double_Antenna_Off()
-    #     uav.Send(data)
-    #     magnetic_declination = uav.uavdata.toward_angle/10
-    #     data =pod.Double_Antenna_On()
-    #     uav.Send(data)
-    #     direction = uav.uavdata.toward_angle/10
-    #     if abs(magnetic_declination - direction) > 3 :
-    #         SelfCheck = 0 
-    #         consolelog("测向自检失败")
-    #     else:
-    #         SelfCheck = 1
-    #         consolelog("测向自检successfull")
+    # 磁罗盘测向和卫星测向偏差
+    if SelfCheck == 1:
+        pod = Fight.Flight_Action()
+        data =pod.MagneticDeclination()
+        uav.Send(data) 
+        data =pod.Double_Antenna_Off()
+        uav.Send(data)
+        magnetic_declination = uav.uavdata.toward_angle/10
+        data =pod.Double_Antenna_On()
+        uav.Send(data)
+        direction = uav.uavdata.toward_angle/10
+        if abs(magnetic_declination - direction) > 3 :
+            SelfCheck = 0 
+            consolelog("测向自检失败")
+        else:
+            SelfCheck = 1
+            consolelog("测向自检successfull")
             
 
-    # # 舱盖状态
-    # if SelfCheck == 1 :
-    #     if airport.airportdata.warehouse_status != 2: 
-    #         SelfCheck =0 
-    #         consolelog("舱盖自检失败")
-    #     else:
-    #         SelfCheck = 1
-    #         consolelog("舱盖自检successfull")
+    # 舱盖状态
+    if SelfCheck == 1 :
+        if airport.airportdata.warehouse_status != 2: 
+            SelfCheck =0 
+            consolelog("舱盖自检失败")
+        else:
+            SelfCheck = 1
+            consolelog("舱盖自检successfull")
 
-    # # 归机机构状态
-    # if SelfCheck == 1 :
-    #     if airport.airportdata.homing_status != 2: 
-    #         SelfCheck =0 
-    #         consolelog("归机机构自检失败")
-    #     else:
-    #         SelfCheck = 1
-    #         consolelog("归位自检successfull")
+    # 归机机构状态
+    if SelfCheck == 1 :
+        if airport.airportdata.homing_status != 2: 
+            SelfCheck =0 
+            consolelog("归机机构自检失败")
+        else:
+            SelfCheck = 1
+            consolelog("归位自检successfull")
 
     # 自检完成  
     if SelfCheck == 1:
@@ -1283,7 +1283,7 @@ class UavReplayThread(threading.Thread):
         a = int('a5',16)
         b = int('5a',16)
         cmd = int('10',16)
-        print("===len "+str(filelen))
+        # print("===len "+str(filelen))
         while self.isStop ==False:
             while self.isPause ==False:
                 # print(self.isStop)
@@ -1293,7 +1293,7 @@ class UavReplayThread(threading.Thread):
                     
                 data = self.f.read(1)
                 currentpos = self.f.tell()
-                print("==="+str(currentpos) +" / "+str(filelen))
+                # print("==="+str(currentpos) +" / "+str(filelen))
                 #播放结束
                 if currentpos >= filelen:
                     self.isStop=True
