ac0a916d2878ff6537a9ee132276af7edd0370e9
magix <minyuanyang@qq.com>
2024-01-28 14:53:34 +0800

    11

--- rpc/doc/sql/uav_fly.sql
+++ rpc/doc/sql/uav_fly.sql
@@ -3,7 +3,7 @@ create table uav_fly
     id       bigint auto_increment comment '路线编号'
         primary key,
     name   varchar(150)                        not null comment '路线昵称',
-    data       varchar(2048)                        not null comment '路线数据',
+    data       varchar(4096)                        not null comment '路线数据',
     create_time       timestamp default CURRENT_TIMESTAMP not null comment '创建时间',
     creator       varchar(16)                        not null comment '创建者'
 )
