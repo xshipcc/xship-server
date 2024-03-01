34566e37d85704ef27d51a345dc0626d3c1fd07d
magix <minyuanyang@qq.com>
2024-03-01 19:48:37 +0800

    更新组播系统

---
+++ drone_projects/webSocket/Multicast.py
@@ -0,0 +1,39 @@
+# coding:utf8
+
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
+multicast_ip = '224.0.0.2'
+port = 8001
+toport =8002
+
+class Multicast(DatagramProtocol):
+    def startProtocol(self):
+        '''
+        加入组播（必须重新）
+        :return: 
+        '''
+        self.transport.setTTL(5) # 设置多播数据包的生存时间
+        self.transport.joinGroup(multicast_ip)
+        self.transport.write('Notify'.encode('utf8'),(multicast_ip,toport))
+
+    def datagramReceived(self, datagram: bytes, addr):
+        '''
+        接收到组播发送的数据
+        :param datagram: 
+        :param addr: 
+        :return: 
+        '''
+        print('Datagram %s received from %s '%(repr(datagram.decode('utf8')),repr(addr)))
+        if datagram.decode('utf8') == 'Notify':
+            self.transport.write('Acknowlege'.encode('utf8'),(multicast_ip,toport))
+
+    def closeConnection(self):
+        '''
+        自定义函数，离开组播时调用
+        :return: 
+        '''
+        self.transport.leaveGroup()
+
+reactor.listenMulticast(port,Multicast(),listenMultiple=True)
+reactor.run()
