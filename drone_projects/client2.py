f115da3c13f840b752fc0e2ce9b1d1414d287522
magix <22333678@qq.com>
2024-03-07 13:54:31 +0800

    1

--- drone_projects/client2.py
+++ drone_projects/client2.py
@@ -1851,7 +1851,7 @@ class UavThread(threading.Thread):
                 if self.iszubo:
                     if(len(databuffer) == 0):
                         data, _ = self.boquue.get()      # buffer size is 4096 bytes
-                        # print(" ：Received message  {}: {}".format(len(data), data))
+                        print(" ：Received message  {}: {}".format(len(data), data))
                         if self.doFlyFile is not None:
                             self.doFlyFile.write(data)
                     else:
