1792eacacc00afad210b93e84af3196a4330d198
magix <22333678@qq.com>
2024-03-08 12:11:09 +0800

    更新

--- rpc/uav/uav.go
+++ rpc/uav/uav.go
@@ -630,7 +630,7 @@ func main() {
 									os.MkdirAll(folderPath, 0777) //0777也可以os.ModePerm
 								}
 
-								letcam := runAI(ctx, dict.RtspUrl, folderPath, "-1", strconv.Itoa(int(dict.Id)), "off", "unsave")
+								letcam := runAI(ctx, dict.RtspUrl, folderPath, "-1", strconv.Itoa(int(dict.Id)), "on", "unsave")
 								ctx.CamAICmd = append(ctx.CamAICmd, letcam)
 
 								fmt.Printf("load ai %d success :\n", dict.Tunnel)
